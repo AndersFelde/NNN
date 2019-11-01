b460671ca5a6ae0836e16f0eb7eccbd2fb563b02
Anders Kvamme Felde <anderskfelde@gmail.com>
2019-11-01 14:56:35 +0100

    Initial commit

---
+++ form.php
@@ -0,0 +1,5 @@
+<form action="#" method="post">
+<input autofocus name="pas" type="password">
+Lagre passord <input type="checkbox" name="stay_signed" checked>
+<button name="login">Logg inn</button>
+</form>

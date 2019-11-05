8cbbde63779a6d525844816f03c218c83690c77e
Anders Kvamme Felde <anderskfelde@gmail.com>
2019-11-05 09:47:55 +0100

    cleaning

--- edit.php
+++ edit.php
@@ -42,35 +42,30 @@ function addRow(rowA){
 };
 
 function delRow(rowD){
-    console.log("mama");
     var element = document.getElementById(rowD);
     element.parentNode.removeChild(element);
 };
 
 </script>
 
-<?php 
+<?php
 session_start();
 
-if(isset($_POST["Edit_all"])){
-    if(isset($_SESSION["signed_in"])){
+if (isset($_POST["Edit_all"])) {
+    if (isset($_SESSION["signed_in"])) {
         $_SESSION["login"] = "true";
         header("Location: edit.php");
     } else {
         include("form.php");
     }
-    
-} elseif(isset($_POST["login"]) || isset($_SESSION["login"])) {
-
-    function printEdit($signed_in){
-        $file = "list.txt";
-        $arr_list = explode("-",file_get_contents($file));
+} elseif (isset($_POST["login"]) || isset($_SESSION["login"])) {
+    function printEdit($signed_in)
+    {
         $arr_list_json = json_decode(file_get_contents("list.json"));
-        $count = count($arr_list) - 1;
         $count = count($arr_list_json)*2;
         #fordi det er to elementer inni hvert objekt
         echo "<form action='#' method='post'>";
-        if($signed_in){
+        if ($signed_in) {
             echo "<br>Fortsatt være logget inn?<input type='checkbox' name='continue_sign_in' checked><br>";
         }
         echo "<button id='endre' name='endring' value='$count' type='submit'>Lagre</button>";
@@ -88,69 +83,51 @@ if(isset($_POST["Edit_all"])){
             echo "<td><input type='number' min='1' max='30' value='$dato' name='dager$i'></td>";
             echo "<td><button type='button' onclick='delRow(" . ($i - 1) . ")'>Slett rad</button></td></tr>";
             $i++;
-          }
+        }
         echo "</tbody>";
         echo "</table>";
         echo "</form>";
         echo "<button id='addRow' onclick='addRow($count)'>Legg til rad</button>";
     };
 
-    if(isset($_POST["login"])){
-        if($_POST["pas"] == "JoeMama"){
-            if(isset($_POST["stay_signed"])){
+    if (isset($_POST["login"])) {
+        if ($_POST["pas"] == "JoeMama") {
+            if (isset($_POST["stay_signed"])) {
                 $_SESSION["signed_in"] = "true";
                 printEdit(true);
             } else {
                 printEdit(false);
             }
-
-            
         } else {
             echo "Feil passord";
             include("form.php");
         }
-    } elseif(isset($_SESSION["signed_in"])){
+    } elseif (isset($_SESSION["signed_in"])) {
         printEdit(true);
     }
     unset($_POST["login"]);
     unset($_SESSION["login"]);
-
-}elseif(isset($_POST["endring"])){
-    $file_content = "";
+} elseif (isset($_POST["endring"])) {
     $json = array();
-    
     for ($i=0; $i < $_POST["endring"]; $i++) {
-        
-        if(empty($_POST["navn$i"]) || empty($_POST["dager" . ($i + 1)])){
+        if (empty($_POST["navn$i"]) || empty($_POST["dager" . ($i + 1)])) {
             $i++;
         } else {
-            $json_obj = new \stdClass;    
+            $json_obj = new \stdClass;
             $json_obj->navn = $_POST["navn$i"];
-            $file_content = $file_content . $_POST["navn$i"] . "-";
-
             $i++;
-
             $json_obj->dato = $_POST["dager$i"];
-            $file_content = $file_content . $_POST["dager" . $i] . "-";
             $json[] = $json_obj;
-            file_put_contents("list.txt", $file_content);
-            
         }
-        
     }
-    var_dump($json);
-    echo "<br>";
-    $json = json_encode($json);
-    file_put_contents("list.json", $json);
-    echo "<br> json: ". $json . "<br>";
-    if(isset($_SESSION["signed_in"])){
-        if(!isset($_POST["continue_sign_in"])){
+    file_put_contents("list.json", json_encode($json));
+    if (isset($_SESSION["signed_in"])) {
+        if (!isset($_POST["continue_sign_in"])) {
             unset($_SESSION["signed_in"]);
         }
-
     }
     echo "du skal ikke være her<br>";
-    #header('Location: index.php');
+    header('Location: index.php');
     echo "<a href='index.php'>index</a>";
 } else {
     header("Location: index.php");
